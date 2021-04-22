6751bb4b8a4b390e0afa9671e58274a18b452bf9
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2021-04-22 19:42:09 -0300

    Preparing for the last HandsOn

--- HandsOn5-6/app/src/main/java/br/com/handson5/database/repository/MovieEntityRepository.kt
+++ HandsOn5-6/app/src/main/java/br/com/handson5/database/repository/MovieEntityRepository.kt
@@ -1,7 +1,20 @@
 package br.com.handson5.database.repository
 
 import br.com.handson5.database.dao.MovieDao
+import br.com.handson5.database.entity.MovieEntity
 
 class MovieEntityRepository(
     private val movieDao: MovieDao
-)
+) {
+    suspend fun insertMovie(movie: MovieEntity) {
+        movieDao.insertMovie(movie)
+    }
+
+    suspend fun getMovies() {
+        movieDao.getMovies()
+    }
+
+    suspend fun deleteMovie(id: Long) {
+        movieDao.deleteMovieById(id)
+    }
+}
